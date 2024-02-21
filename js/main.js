53f588f9d790fc9a58402400dd6cda5da8c89847
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2024-02-21 09:42:58 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,4 +11,22 @@ $(function() {
 	    $('.bg-header-desktop').removeClass('active');
 	   }
 	});
+	// Profile JS
+	$(".btn-profile-add-to-cart-donate-now > li.profile-li > a").click(function() {
+		$(this).parent().parent().find('ul').slideUp(700);
+		$(".btn-profile-add-to-cart-donate-now > li:first-child > a").parent().removeClass("active");
+		if(!$(this).next().is(":visible")) {
+				$(this).next().slideDown(700);
+				$(this).parent().addClass("active");
+		}
+	});
+	// Shopping Listing JS
+	$(".btn-profile-add-to-cart-donate-now > li.shopping-cart-li > a").click(function() {
+		$(this).parent().parent().find('ul').slideUp(700);
+		$(".btn-profile-add-to-cart-donate-now > li:nth-child(2) > a").parent().removeClass("active");
+		if(!$(this).next().is(":visible")) {
+				$(this).next().slideDown(700);
+				$(this).parent().addClass("active");
+		}
+	});
 });
